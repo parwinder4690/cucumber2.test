0794b04ed7be5654603687af1fb4c8f7cfe86679
Parwinder Kaur <parwinder4690@gmail.com>
2017-10-28 12:31:20 -0400

    Adding new cucumber project example

---
+++ createGit.bat
@@ -0,0 +1,6 @@
+echo "# cucumber.test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/parwinder4690/cucumber2.test.git
+git push -u origin master
